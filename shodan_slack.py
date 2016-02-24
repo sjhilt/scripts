89b6e54bea6eeb6e4a8f2d481a8688ed7a5c50ec
Stephen Hilt <shooter000@gmail.com>
2016-02-24 11:19:03 -0500

    Update shodan_slack.py

--- shodan_slack.py
+++ shodan_slack.py
@@ -12,16 +12,16 @@ slack = Slacker('SLACK_API_KEY')
 
 try:
         # Setup the api
-        api = shodan.Shodan(API_KEY):
+        api = shodan.Shodan(API_KEY)
 
         # Perform the search
         total = api.count('has_screenshot:true')['total']
         page = randint(1, total / 100 + 1)
         results = api.search('has_screenshot:true', page=page)
         num_results = len(results['matches'])
-        shodan_result = results['matches'][randint(0,num_results)]['ip_str']
+        shodan_result = results['matches'][randint(0,num_results)]['ip_str']        
         SHODAN_MESSAGE = "https://www.shodan.io/host/{}/image".format(shodan_result)
-        # Change all the variables you need before running such as BOTNAME and YOUR_EMOJI
+
         slack.chat.post_message('#random', "Here is a Random host with a screeshot from SHODAN {}".format(SHODAN_MESSAGE), username="BOTNAME", icon_emoji="YOUR_EMOJI")
 except Exception as e:
         print 'Error: %s' % e
