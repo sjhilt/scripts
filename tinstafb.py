5c5107bbc510445ce7a9cf415ac040650f709029
Stephen Hilt <shooter000@gmail.com>
2016-09-26 11:06:01 -0400

    Create tinstafb.py

---
+++ tinstafb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Author: Stephen J. Hilt
+#	pour beer from Talos' Automated Kegorator 
+# from DerbyCon 6.0
+#
+#
+#########################################################
+from pycomm.ab_comm.slc import Driver as SlcDriver
+import time
+
+# Argument Checking
+if (len(sys.argv) != 2): 
+	print("USAGE: python ./tinstafb.py <host> <left|right|lock>")
+	sys.exit()
+host=sys.argv[1]
+command=sys.argv[2]
+
+c = SlcDriver()
+if c.open(host):
+
+  if command == "left":
+    # Left tap on
+    c.write_tag('B3:0/0', 1)
+    # Pour the perfect beer
+    time.sleep(5)
+    # left tap off
+    c.write_tag('B3:0/0', 0)
+  elif command == "right":
+    # right tap on
+    c.write_tag('B3:0/1', 1)
+    # Pour the perfect beer
+    time.sleep(5)
+    c.write_tag('B3:0/1', 0)
+  elif command == "lock"
+    c.write_tag('B3:0/2', 1)
+  else:
+    # Spencer mode on!
+    c.write_tag('B3:0/3', 1)
+
+c.close()
