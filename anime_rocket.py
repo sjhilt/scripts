f360ebbac8906166a2045d4b53830f77a60257bb
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2021-06-16 15:05:06 -0400

    Create anime_rocket.py
    Anime Quote to Rocket.Chat example

---
+++ anime_rocket.py
@@ -0,0 +1,13 @@
+from requests import sessions
+from pprint import pprint
+import requests
+from rocketchat_API.rocketchat import RocketChat
+
+username = ''
+password = ''
+rocket = RocketChat(username, password, server_url='rocket.chat:port' )
+url = 'https://animechan.vercel.app/api/random'
+headers = {'user-agent':'My Library (YOUR GITHUB PAGE)'}
+response = requests.get(url, headers=headers)
+results = response.json()
+rocket.chat_post_message("%s -- %s" % (results['quote'],results['character']), channel='GENERAL', alias='%s' % results['anime'], emoji=':japan:').json()
