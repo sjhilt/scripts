c2be0da853c5639704b8e247e20ef999d4d53f78
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2026-07-06 09:53:35 -0400

    Update anime_rocket.py

--- anime_rocket.py
+++ anime_rocket.py
@@ -6,7 +6,7 @@ from rocketchat_API.rocketchat import RocketChat
 username = ''
 password = ''
 rocket = RocketChat(username, password, server_url='rocket.chat:port' )
-url = 'https://animechan.vercel.app/api/random'
+url = 'https://api.animechan.io/v1/quotes/random'
 headers = {'user-agent':'My Library (YOUR GITHUB PAGE)'}
 response = requests.get(url, headers=headers)
 results = response.json()
